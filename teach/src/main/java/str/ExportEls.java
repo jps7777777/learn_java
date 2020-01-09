7d13947d43ecad0c72c02b564f90aaa9d446fd51
jps7777777 <841590610@qq.com>
2020-01-09 18:03:49 +0800

    20200109

--- teach/src/main/java/str/ExportEls.java
+++ teach/src/main/java/str/ExportEls.java
@@ -6,6 +6,11 @@ import com.alibaba.druid.util.StringUtils;
 import org.apache.poi.hssf.usermodel.HSSFRow;
 import org.apache.poi.hssf.usermodel.HSSFSheet;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
 
 import java.io.*;
 import java.text.SimpleDateFormat;
@@ -65,9 +70,49 @@ public class ExportEls {
     }
 
     /**
+     * 读Excel文件
+     */
+    public List<String[][]> method(){
+        File xlsFile = new File("D:\\demo_use\\els\\poi.xls");
+        // 获得工作簿
+        Workbook workbook = null;
+        try {
+            workbook = WorkbookFactory.create(xlsFile);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        List<String[][]> list = new ArrayList<>();
+        // 获得工作表个数
+        int sheetCount = workbook.getNumberOfSheets();
+        // 遍历工作表
+        for (int i = 0; i < sheetCount; i++) {
+            Sheet sheet = workbook.getSheetAt(i);
+            // 获得行数
+            int rows = sheet.getLastRowNum() + 1;
+            // 获得列数，先获得一行，在得到该行列数
+            Row tmp = sheet.getRow(0);
+            if (tmp == null) {
+                continue;
+            }
+            int cols = tmp.getPhysicalNumberOfCells();
+            String[][] table = new String[rows][cols];
+            // 读取数据
+            for (int row = 0; row < rows; row++) {
+                Row r = sheet.getRow(row);
+                for (int col = 0; col < cols; col++) {
+                    System.out.printf("%10s", r.getCell(col).getStringCellValue());
+                }
+                System.out.println();
+            }
+            list.add(table);
+        }
+        return list;
+    }
+
+    /**
      *
      */
-    public void method() {
+    public void method1() {
         File file = new File("D:\\demo_use\\files\\file.txt");
         String[] value = new String[13];
         String[] titile = new String[13];
